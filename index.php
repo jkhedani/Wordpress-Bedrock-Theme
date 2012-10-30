44788d77001b8269ee05790a6a8582adbf275d5e
jkhedani <jkhedani@hawaii.edu>
2012-10-29 18:23:34 -1000

    Modify DOM structure to incorporate Bootstrap

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * The main template file. (also will serve as default 'home')
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package _s
@@ -13,8 +8,8 @@
  */
 
 get_header(); ?>
-
-		<div id="primary" class="content-area">
+	<div class="row-fluid"><!-- Bootstrap: REQUIRED! -->
+		<div id="primary" class="content-area span8">
 			<div id="content" class="site-content" role="main">
 
 			<?php if ( have_posts() ) : ?>
@@ -44,6 +39,6 @@ get_header(); ?>
 
 			</div><!-- #content .site-content -->
 		</div><!-- #primary .content-area -->
-
-<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
+	</div><!-- .row -->
 <?php get_footer(); ?>
