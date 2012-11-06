11c8fa1e8746b9641a0f6df3cab724ffb76e3fe4
jkhedani <jkhedani@hawaii.edu>
2012-11-05 20:01:34 -1000

    Create home page sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,22 @@
  * @since _s 1.0
  */
 ?>
+	<?php if( is_home() ) { ?>
+		<div id="intro-banner" class="<?php echo get_theme_mod('courses_branding_tint', 'default_value'); ?> span4" role="complementary">
+			<?php
+				echo '<div id="college-branding" class="';
+				echo get_theme_mod('courses_branding_college_affil', 'default_value') . ' ';
+				echo get_theme_mod('courses_branding_tint', 'default_value');
+				echo '"></div>';
+			?>
+			<div id="intro-banner-content">
+				<span class="welcome">Welcome To</span>
+				<h1><?php bloginfo('name'); ?></h1>
+				<?php echo '<p>'.get_theme_mod( 'courses_short_desc', 'default_value' ).'</p>'; ?>
+			</div>
+			<div id="alternate-brand"></div>
+		</div>
+	<?php } else { ?>
 		<div id="secondary" class="widget-area span4" role="complementary">
 			<?php do_action( 'before_sidebar' ); ?>
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
@@ -32,3 +48,4 @@
 
 			<?php endif; // end sidebar widget area ?>
 		</div><!-- #secondary .widget-area -->
+	<?php } // end is_home() ?>
