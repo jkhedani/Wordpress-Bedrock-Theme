1f36051c63a6252b093234573a78c9685d3596ed
jkhedani <jkhedani@hawaii.edu>
2012-11-05 19:59:38 -1000

    Update home page layout

--- front-page.php
+++ front-page.php
@@ -10,16 +10,15 @@
 get_header(); ?>
 
 	<div class="row-fluid"><!-- Bootstrap: REQUIRED! -->
+		<?php get_sidebar(); ?>
 		<div id="primary" class="content-area span8">
 			<div id="content" class="site-content" role="main">
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php _s_content_nav( 'nav-above' );
-
-				// DCDC Module List
+				<?php // DCDC Module List
 				$moduleListContent = new WP_Query ( array(
 					'post_type' => 'units',
+					'order' => 'ASC',
+					'orderby' => 'menu_order'
 				));
 				
 				p2p_type( 'units_to_modules' )->each_connected( $moduleListContent, array(), 'modules' );
@@ -29,49 +28,29 @@ get_header(); ?>
 					
 					p2p_type( 'modules_to_lessons' )->each_connected( $post->modules, array(), 'lessons' );
 					$unitTitle = get_the_title();
-					echo '<p>Parent Unit:</p>';
-						echo $unitTitle;
-					echo '<ul>';
+					echo '<ol class="unit"><li>' . $unitTitle;
+					echo '<ol class="module">';
 					foreach ( $post->modules as $post ) : setup_postdata( $post );
 					$moduleTitle = get_the_title();
 					echo		'<li>' . $moduleTitle;
-					echo 			'<ul>';
+					echo 			'<ol class="lesson">';
 					foreach ( $post->lessons as $post ) : setup_postdata( $post );
 					$lessonTitle = get_the_title();
 					echo 				'<li>' . $lessonTitle . '</li>';
 					endforeach; // end Lessons
-					echo 			'</ul>';
+					echo 			'</ol>'; // .lesson
 					endforeach; // end Modules
-					echo '</li></ul>';
+					echo '</li></ol>'; // .module
+					echo '</li></ol>'; // .unit
 					wp_reset_postdata();
 
 				endwhile;
 				wp_reset_postdata();
 
-				/* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php _s_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'no-results', 'index' ); ?>
-
-			<?php endif; ?>
+				?>
 
 			</div><!-- #content .site-content -->
 		</div><!-- #primary .content-area -->
-		<?php get_sidebar(); ?>
 	</div><!-- .row -->
 	
 <?php get_footer(); ?>
