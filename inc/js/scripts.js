9819947b15815b3565ec77eb2682b7290ea09760
jkhedani <jkhedani@hawaii.edu>
2012-11-26 19:15:25 -1000

    Update equal heights plugin, create conditionals for running various js

--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -1,22 +1,40 @@
 jQuery(document).ready(function($){
 
 	// Equal Heights plugin
-	// By: Scott Jehl
-	// Modified by: Justin Hedani
-	$.fn.equalHeights = function(px) {
+	// By: Justin Hedani
+	// Based on a plugin by: Scott Jehl
+	$.fn.equalHeights = function() {
 		$(this).each(function(){
-			var currentTallest = 0;
+			var currentTallestOne = 0;
 			$(this).find('.module-title').each(function(i){
-				if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+				if ($(this).height() > currentTallestOne) { currentTallestOne = $(this).height(); }
 			});
-			//if (!px || !Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
-			// for ie6, set height since min-height isn't supported
-			if ($.browser.msie && $.browser.version == 6.0) { $(this).find('.module-title').css({'height': currentTallest}); }
-			$(this).find('.module-title').css({'height': currentTallest}); // 20120130 - changed value to 'height'
+			$(this).find('.module-title').css({'height': currentTallestOne}); // 20120130 - changed value to 'height'
+		});
+
+		$(this).each(function(){
+			var currentTallestTwo = 0;
+			$(this).find('.module-content').each(function(i){
+				if ($(this).height() > currentTallestTwo) { currentTallestTwo = $(this).height(); }
+			});
+			$(this).find('.module-content').css({'height': currentTallestTwo}); // 20120130 - changed value to 'height'
 		});
 		return this;
 	};
 	
 	// Make it so that the heights of the Single Module layouts all have equal heights
-	$('body.modules-lessons.single-layout #main ol.modules').equalHeights();
+	if ($('body').hasClass('home')) {
+		$('body.modules-lessons.single-layout #main ol#singularModulesLessons').equalHeights();
+	}
+
+	//	# UI Scripts #
+	//	Hide/Show Edit Link for...
+	if ($('body').hasClass('home') && $('body').hasClass('logged-in')) {
+		$('#intro-banner').bind('mouseenter mouseleave', function(){ $(this).find('a.edit-post-link').toggle(); }); // Home Page
+		$('li.module').bind('mouseenter mouseleave', function(){ $(this).find('a.edit-post-link').not('li.lesson a.edit-post-link').toggle(); }); // Modules
+		$('li.lesson').bind('mouseenter mouseleave', function(){
+			$(this).find('a.edit-post-link').toggle(); // Lessons
+			$(this).closest('li.module').find('div.module-content-wrapper + a.edit-post-link').toggle(); // Hide Module edit when over a lesson
+		});
+	}
 });
