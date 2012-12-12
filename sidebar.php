6cb89c349eb501565789ce58518d2eb3882e609f
jkhedani <jkhedani@hawaii.edu>
2012-12-11 16:30:17 -1000

    Update inner page sidebar content. Add sidebar nav for module and lesson pages

--- sidebar.php
+++ sidebar.php
@@ -9,61 +9,57 @@
 	<?php if( is_home() ) { ?>
 		<div id="intro-banner" class="intro-banner span4" role="complementary">
 			<?php
-				echo '<div class="college-branding ';
-				echo get_theme_mod('courses_branding_college_affil', 'default_value') . ' ';
-				echo '"></div>';
+				echo '<div class="college-branding" data-affil="'.get_theme_mod('courses_branding_college_affil', 'default_value').'"></div>';
 			?>
 			<div class="intro-banner-content">
-				<span class="welcome">Welcome To</span>
+				<span class="welcome">Welcome to</span>
 				<h1><?php bloginfo('name'); ?></h1>
 				<?php echo '<p>'.get_theme_mod( 'courses_short_desc', 'default_value' ).'</p>'; ?>
-				<?php echo '<img src="http://placehold.it/250x275" alt="placeholder" />'; ?>
+				<?php echo '<img src="http://placehold.it/260x275" alt="placeholder" />'; ?>
 			</div>
 			<div class="alternate-brand"></div>
 			<?php if (is_user_logged_in()) { echo '<a class="edit-post-link" href="'.site_url().'/wp-admin/customize.php" title="Edit the '.get_the_title().' module">Edit this module</a>'; } ?>
 		</div>
 	<?php } else { ?>
-		<div id="secondary" class="widget-area span5" role="complementary">
+		<div id="secondary" class="widget-area span4 offset1" role="complementary">
 			<?php do_action( 'before_sidebar' ); ?>
 			
 			<?php // Module/Lesson Sidebar
-			
-			// Breadcrumbs
-			echo '<div class="breadcrumbs">';
-			echo 	'<ol>';
-			echo 	'<li><a href="'.get_home_url().'"></a>';
-			echo 	'</ol>';
-			echo '</div>';
 
 			// Module/Lesson Image
 			if (get_the_post_thumbnail()) {
 				echo get_the_post_thumbnail();
 			} else {
-				echo '<img src="http://placehold.it/257x128" alt="placeholder" />';
+				echo '<img src="http://placehold.it/300x171" alt="placeholder" />';
 			}
 			
-			echo '<hr />';
+			//echo '<hr />';
 
 			if(!(get_theme_mod('courses_layout_ia') == 'unitsModulesLessons')) {
 
 				if(get_post_type() == 'modules') {
-					echo '<h3>'.get_the_title().'</h3>';
+					echo '<h3 class="sidebar-nav-title">'.get_the_title().'</h3>';
 					$sidebarNavContent = new WP_Query ( array(
 						'connected_type' => 'modules_to_lessons',
-						'connected_items' => get_queried_object_id(),
+						'connected_items' => get_queried_object(),
 					));
 
+					echo '<ol class="lessons">';
 					if ($sidebarNavContent->have_posts()) {
-						echo '<ol class="lessons">';
 						while($sidebarNavContent->have_posts()) : $sidebarNavContent->the_post();
-							echo '<li class="lesson"><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+							echo '<li class="lesson"><a class="lesson-link" href="'.get_permalink().'">'.get_the_title().'</a></li>';
 						endwhile;
-						echo '</ol>'; // end lessons
-					} else { // If there are no lessons
-						echo '<p><a href="'.admin_url( 'post-new.php?post_type=lessons' ).'" title="Create a lesson in this module.">Create</a> or ';
-						echo '<a href="'.admin_url('edit.php?post_type=lessons').'" title="Attach a lesson to this module.">attach</a> a lesson.</p>';
+						wp_reset_postdata();
+						
 					}
+					if (is_user_logged_in()) {
+						echo '<li class="lesson no-content"><a class="btn btn-primary" href="'.admin_url( 'post-new.php?post_type=lessons' ).'" title="Create a lesson in this module.">Create</a> or ';
+						echo '<a class="btn btn-primary" href="'.admin_url('edit.php?post_type=lessons').'" title="Attach a lesson to this module.">attach</a> a lesson.</li>';
+					}
+					echo '</ol>'; // end lessons
+
 				} elseif (get_post_type() == 'lessons') {
+
 					$currentPostID = $post->ID;
 					$sidebarNavContent = new WP_Query ( array(
 						'connected_type' => 'modules_to_lessons',
@@ -73,7 +69,7 @@
 					if ($sidebarNavContent->have_posts()) {
 
 						while($sidebarNavContent->have_posts()) : $sidebarNavContent->the_post();
-							echo '<h3><a href="'.get_permalink().'">'.get_the_title().'</a></h3>';
+							echo '<h3 class="sidebar-nav-title"><a href="'.get_permalink().'">'.get_the_title().'</a></h3>';
 							$parentID = $post->ID;
 							$getConnectedToParent = new WP_Query( array(
 								'connected_type' => 'modules_to_lessons',
@@ -84,7 +80,7 @@
 								echo '<ol class="lessons">';
 								while($getConnectedToParent->have_posts()) : $getConnectedToParent->the_post();
 									$postID = $post->ID;
-									echo '<li class="lesson"><a ';
+									echo '<li class="lesson"><a class="lesson-link" ';
 									if ($currentPostID == $postID) {
 										$postType = get_post_type($postID);
 										$postTypeObject = get_post_type_object($postType);
@@ -99,6 +95,7 @@
 						wp_reset_postdata();
 					 // end lessons
 					}
+			
 				}
 			} else {
 				// Units, Modules & Lessons call
