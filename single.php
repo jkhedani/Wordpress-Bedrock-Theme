3c5a60e3c87f7d82f1d4f22e1d282d1aeab50fe4
jkhedani <jkhedani@hawaii.edu>
2012-12-11 16:30:41 -1000

    Add proper connections for content page structure

--- single.php
+++ single.php
@@ -14,11 +14,80 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php //_s_content_nav( 'nav-above' ); ?>
+				<?php dcdc_get_breadcrumbs(); ?>
+
+				<?php
+
+					if(!(get_theme_mod('courses_layout_ia') == 'unitsModulesLessons')) {
+						$currentPostType = get_post_type();
+						$currentPostID = $post->ID;
+						// Count Modules or Lessons
+						if ($currentPostType == 'modules') {
+							$getUnitConnection = new WP_Query ( array(
+								'connected_type' => 'units_to_modules',
+								'connected_items' => get_queried_object(),
+							));
+
+							while($getUnitConnection->have_posts()) : $getUnitConnection->the_post();
+								$i = 0;
+								$moduleCount = new WP_Query ( array(
+									'connected_type' => 'units_to_modules',
+									'connected_items' => $post->ID,
+								));
+
+								while($moduleCount->have_posts()) : $moduleCount->the_post();
+									$postID = $post->ID;
+									if($currentPostID == $postID) {
+										echo '<span class="moduleCount">Module ' .$i. '</span>';
+									}
+									$i++;
+								endwhile;
+								wp_reset_postdata();
+
+							endwhile;
+							wp_reset_postdata();
+
+						} else if ($currentPostType == 'lessons') {
+							// if lessons
+						}
+					} else {
+						// If unitsModulesLessons
+						$currentPostType = get_post_type();
+						$currentPostID = $post->ID;
+						// Count Modules or Lessons
+						if ($currentPostType == 'modules') {
+							$getUnitConnection = new WP_Query ( array(
+								'connected_type' => 'units_to_modules',
+								'connected_items' => get_queried_object(),
+							));
+
+							while($getUnitConnection->have_posts()) : $getUnitConnection->the_post();
+								$i = 0;
+								$moduleCount = new WP_Query ( array(
+									'connected_type' => 'units_to_modules',
+									'connected_items' => $post->ID,
+								));
+
+								while($moduleCount->have_posts()) : $moduleCount->the_post();
+									$postID = $post->ID;
+									if($currentPostID == $postID) {
+										echo 'Module' . $i;
+									}
+									$i++;
+								endwhile;
+								wp_reset_postdata();
+
+							endwhile;
+							wp_reset_postdata();
+
+						} else if ($currentPostType == 'lessons') {
+							// if lessons
+						}
+					}
+
+				?>
 
 				<?php get_template_part( 'content', 'single' ); ?>
-
-				<?php //_s_content_nav( 'nav-below' ); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
@@ -32,7 +101,8 @@ get_header(); ?>
 		</div><!-- #primary .content-area -->
 		<?php get_sidebar(); ?>
 	</div><!-- .row-fluid -->
-
-	<?php dcdc_get_pager(); ?>
+	<footer class="entry-meta span10">
+		<?php dcdc_get_pager(); ?>
+	</footer>
 
 <?php get_footer(); ?>
