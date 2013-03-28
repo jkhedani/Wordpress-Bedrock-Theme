193aba305470739f20f5ff6d2ea371a71c86de73
jkhedani <jkhedani@hawaii.edu>
2013-03-27 14:25:06 -1000

    Require one template in childtheme (index.php)

---
+++ samplechildtheme/index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The main template file. (also will serve as default 'home')
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _s
+ * @since _s 1.0
+ */
+
+get_header(); ?>
+
+	<div class="row-fluid"><!-- Bootstrap: REQUIRED! -->
+		<div id="primary" class="content-area span8">
+			<div id="content" class="site-content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php _s_content_nav( 'nav-above' ); ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+				<?php _s_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'no-results', 'index' ); ?>
+
+			<?php endif; ?>
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+		<?php get_sidebar(); ?>
+	</div><!-- .row -->
+	
+<?php get_footer(); ?>
