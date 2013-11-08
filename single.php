bb003970e432d50e4a4065925a3d0c3b5904e301
jkhedani <jkhedani@hawaii.edu>
2013-11-07 16:13:51 -1000

    Ensure all template tag calls for bedrock_ base are removed.

--- single.php
+++ single.php
@@ -8,39 +8,27 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area row">
-		<div id="content" class="site-content span8" role="main">
+	<div id="content" class="site-content" role="main">
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'templates/content', 'single' ); ?>
 		
-		<?php bedrock_contentstart(); ?>
-
-		<?php bedrock_get_breadcrumbs(); ?>
-
-		<?php bedrock_abovepostcontent(); ?>
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'templates/content', 'single' ); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template( '', true );
-			?>
-
-		<?php endwhile; // end of the loop. ?>
-
 		<?php
-			// "Back To Top" Link
-			if(!$post->post_content == "")
-				echo '<a id="backToTop" href="" title="Scroll to the top of this page.">Back To Top</a>';
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || '0' != get_comments_number() )
+				comments_template( '', true );
 		?>
 
-		<?php bedrock_belowpostcontent(); ?>
+	<?php endwhile; // end of the loop. ?>
 
-		<?php bedrock_contentend(); ?>
+	<?php
+		// "Back To Top" Link
+		if(!$post->post_content == "")
+			echo '<a id="backToTop" href="" title="Scroll to the top of this page.">Back To Top</a>';
+	?>
 
-		</div><!-- #content .site-content -->
-	</div><!-- #primary .content-area -->
+	</div><!-- #content .site-content -->
 	
 	<?php get_sidebar(); ?>
 
